35f04b707626118ebe84bd263f566359f13d794d
Michael <numtrixteam@gmail.com>
2025-08-04 19:48:21 -0500

    update

--- js/background.js
+++ js/background.js
@@ -4,7 +4,12 @@ function drawBackground() {
     background(135, 206, 235);
     
     // TODO: Sun on the right side
-    
+fill(255, 255, 0);
+   ellipse(0, 0, 100, 100,); 
+   fill(128, 128, 128); // grey color
+   rect (350, 250,)
+
+   
     // Grass at the bottom (100px high)
     fill(34, 139, 34);
     rect(0, 400 - 100, 400, 100);
