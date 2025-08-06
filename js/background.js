771196831de01d055b3788a2af30926dc22ae665
Michael <numtrixteam@gmail.com>
2025-08-05 21:13:42 -0500

    updatebackground

--- js/background.js
+++ js/background.js
@@ -7,8 +7,14 @@ function drawBackground() {
 fill(255, 255, 0);
    ellipse(0, 0, 100, 100,); 
    fill(128, 128, 128); // grey color
-   rect (350, 250,)
-
+   rect (350, 250,50, 50,)
+   rect (350, 250,50, 50,)
+   rect (350, 250, 20, 20,);
+   rect (300, 250,50, 50)
+   rect (350, 250,50, 50,)
+   rect (330, 250, 20, 20,);
+   rect (330, 200,50, 50)
+   fill(255, 255, 255);
    
     // Grass at the bottom (100px high)
     fill(34, 139, 34);
